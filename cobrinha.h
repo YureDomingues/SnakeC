8635ad055c81905ad2a98f91ebb138a1158781f0
Yure Domingues <yure.domingues@gmail.com>
2022-10-19 13:10:58 -0300

    Sexto commit

---
+++ cobrinha.h
@@ -0,0 +1,32 @@
+#ifndef _COBRINHA_H
+#define _COBRINHA_H
+
+#define CABECA_COBRA 'C'
+#define CORPO_COBRA 'O'
+#define COMIDINHA 'x'
+
+#define CIMA 'w'
+#define ESQUERDA 'a'
+#define BAIXO 's'
+#define DIREITA 'd'
+
+#include "mapa.h"
+
+typedef struct cobrinha{
+    char partes[100+1];
+    POSICAO pos_partes[100];
+    int tam_calda;
+}COBRA;
+
+
+void inicializar_partes();
+void acabou();
+int direcao_valida(char direcao);
+void spawn_objeto(char objeto);
+int comeu(int origem_x, int origem_y, int destino_x, int destino_y);
+void cresce_cobra(int x, int y);
+int ParteCorpo(int parte);
+
+
+
+#endif
